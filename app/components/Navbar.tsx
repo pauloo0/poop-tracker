db8fe2ee6be5737e58144545ced14bbe012b31ed
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2025-03-11 13:58:54 +0000

    Create basic navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+
+function Navbar({ active }: { active: string }) {
+  const activeClass = 'text-highlight font-bold text-lg'
+
+  return (
+    <nav className='fixed inset-x-0 bg-primary bottom-0 z-10 flex justify-around py-4 px-2'>
+      <Link className={active === 'home' ? activeClass : ''} href='/dashboard'>
+        Home
+      </Link>
+      <Link
+        className={active === 'leaderboard' ? activeClass : ''}
+        href='/leaderboard'
+      >
+        Leaders
+      </Link>
+      <Link
+        className={active === 'new-poop' ? activeClass : ''}
+        href='/new-poop'
+      >
+        New Poop
+      </Link>
+      <Link className={active === 'logs' ? activeClass : ''} href='/logs'>
+        Logs
+      </Link>
+      <Link className={active === 'profile' ? activeClass : ''} href='/profile'>
+        Profile
+      </Link>
+    </nav>
+  )
+}
+
+export default Navbar
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from '../components/Navbar'
+
 export default function Dashboard() {
   return (
     <>
+      <Navbar active='home' />
       <h1>Dashboard</h1>
     </>
   )
