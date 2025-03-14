e01ffab1060f7b2772fed6b32cb29eaa49a51d0f
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2025-03-14 11:02:48 +0000

    change alignment in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ function Navbar({ active }: { active: string }) {
   const activeClass = 'text-highlight font-bold text-lg'
 
   return (
-    <nav className='fixed inset-x-0 bg-primary bottom-0 z-10 flex justify-around py-4 px-2'>
+    <nav className='fixed inset-x-0 bg-primary bottom-0 z-10 flex items-center justify-around py-4 px-2'>
       <Link className={active === 'home' ? activeClass : ''} href='/dashboard'>
         Home
       </Link>
