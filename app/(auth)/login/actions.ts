df83fc4c396af6c380eabcd4f4eecb9415011328
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2025-03-22 22:56:16 +0000

    Add login system

--- app/(auth)/login/actions.ts
+++ app/(auth)/login/actions.ts
@@ -1,8 +1,22 @@
 'use server'
-import { redirect } from 'next/navigation'
 
-export async function loginUser(formData: FormData) {
-  console.log(formData)
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '@/app/lib/firebase'
 
-  redirect('/dashboard')
+export async function loginUserEmailPassword(formData: FormData) {
+  const loggedUser = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  try {
+    await signInWithEmailAndPassword(
+      auth,
+      loggedUser.email,
+      loggedUser.password
+    )
+  } catch (error) {
+    console.error('Error in loginUserEmailPassword: ', error)
+    throw error
+  }
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,23 +1,78 @@
+'use client'
+
 import Form from 'next/form'
 import Link from 'next/link'
-import { loginUser } from './actions'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { useTransition, useState } from 'react'
+
+import { auth, googleProvider } from '@/app/lib/firebase'
+import { loginUserEmailPassword } from './actions'
+import { signInWithPopup } from 'firebase/auth'
+
+async function loginUserGoogle() {
+  try {
+    await signInWithPopup(auth, googleProvider)
+  } catch (error) {
+    console.error('Google Login error: ', error)
+    throw error
+  }
+}
 
 export default function Login() {
   const input_group = 'flex flex-col'
   const input_group_label = 'px-1 text-foreground'
   const input_group_input = 'bg-background py-2 px-3 border-b-2 border-primary'
 
+  const router = useRouter()
+
+  const [isPending, startTransition] = useTransition()
+  const [error, setError] = useState<string | null>(null)
+
+  const handleEmailSubmit = (formData: FormData) => {
+    startTransition(async () => {
+      try {
+        await loginUserEmailPassword(formData)
+        router.push('/dashboard')
+      } catch (error) {
+        setError(
+          error instanceof Error
+            ? error.message
+            : 'Failed to sign up with email/password.'
+        )
+      }
+    })
+  }
+
+  const handleGoogleLogin = () => {
+    startTransition(async () => {
+      try {
+        await loginUserGoogle()
+        router.push('/dashboard')
+      } catch (error) {
+        setError(
+          error instanceof Error
+            ? error.message
+            : 'Failed to sign up with Google.'
+        )
+      }
+    })
+  }
+
   return (
-    <Form action={loginUser} className='flex flex-col gap-6 w-full sm:w-96 p-6'>
+    <Form
+      action={handleEmailSubmit}
+      className='flex flex-col gap-6 w-full sm:w-96 p-6'
+    >
       <div className={input_group}>
-        <label htmlFor='username' className={input_group_label}>
-          Username
+        <label htmlFor='email' className={input_group_label}>
+          Email
         </label>
         <input
           type='text'
-          id='username'
-          name='username'
-          placeholder='Username'
+          id='email'
+          name='email'
+          placeholder='Email'
           className={input_group_input}
         />
       </div>
@@ -35,9 +90,24 @@ export default function Login() {
         />
       </div>
 
-      <button className='bg-primary text-foreground py-2 rounded-md'>
-        Login
-      </button>
+      <div className='flex flex-col gap-4'>
+        <button
+          type='submit'
+          disabled={isPending}
+          className='bg-primary text-foreground py-3 px-6 rounded-2xl flex flex-row items-center justify-start gap-6'
+        >
+          {isPending ? 'Logging in...' : 'Login'}
+        </button>
+        <button
+          disabled={isPending}
+          onClick={handleGoogleLogin}
+          className='bg-white text-black py-3 px-6 rounded-2xl flex flex-row items-center justify-start gap-6'
+        >
+          <Image src='/google-logo.webp' alt='' width={24} height={24} />
+          {isPending ? 'Logging in...' : 'Login with Google'}
+        </button>
+      </div>
+      {error && <p className='text-error text-center'>{error}</p>}
       <p>
         {`Don't have an account? `}{' '}
         <Link className='text-primary hover:underline' href={`/register`}>
