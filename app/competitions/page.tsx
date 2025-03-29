19a290df5496d51d460a7906ec809a14c1f10cbe
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2025-03-29 23:42:56 +0000

    Competitions page is now dynamic

--- app/competitions/page.tsx
+++ app/competitions/page.tsx
@@ -2,24 +2,71 @@
 
 import { Pencil, Plus } from 'lucide-react'
 import Link from 'next/link'
-import { useSearchParams } from 'next/navigation'
-import { useState } from 'react'
+import Loading from '@/app/components/Loading'
+import { useSearchParams, useRouter } from 'next/navigation'
+import { useState, useEffect } from 'react'
 import { useAuthState } from '@/app/hooks/useAuthState'
 import { useAuthRedirect } from '@/app/hooks/useAuthRedirect'
+import { Competition } from '@/app/lib/types'
+import { getCompetitions } from '@/app/competitions/new/actions'
 
 export default function Competitions() {
   const { loading: authLoading, user } = useAuthState()
   useAuthRedirect({ loading: authLoading, user })
 
+  const router = useRouter()
   const searchParams = useSearchParams()
-  const initialCompetition = searchParams.get('competition') || 'none'
+  const competitionId = searchParams.get('competition') || 'none'
+
+  const [error, setError] = useState<string | null>(null)
+  const [dataLoading, setDataLoading] = useState<boolean>(true)
+  const [competitions, setCompetitions] = useState<Competition[] | null>(null)
 
   const [selectedCompetition, setSelectedCompetition] =
-    useState<string>(initialCompetition)
-  const [competitionName, setCompetitionName] =
-    useState<string>('Poopions League')
+    useState<Competition | null>(null)
+
+  const [competitionName, setCompetitionName] = useState<string>('')
+  const [canEditCompetition, setCanEditCompetition] = useState<boolean>(false)
   const [isEditingCompetitionName, setIsEditingCompetitionName] =
     useState(false)
+
+  useEffect(() => {
+    if (!user) return
+
+    const fetchCompetition = async () => {
+      try {
+        const userId: string = user.uid
+        const resCompetition = await getCompetitions(userId)
+        const { success, data } = resCompetition
+
+        if (!success || !data) throw new Error('Competition not found.')
+
+        setCompetitions(data)
+        setDataLoading(false)
+      } catch (error) {
+        console.error(error)
+        setError('Error fetching competition data')
+      }
+    }
+
+    fetchCompetition()
+  }, [user, competitionId])
+
+  useEffect(() => {
+    if (!user || !competitions) return
+
+    const selectedCompetition = competitions.find(
+      (competition) => competition.id === competitionId
+    )
+
+    if (selectedCompetition) {
+      setSelectedCompetition(selectedCompetition)
+      setCanEditCompetition(selectedCompetition.createdBy === user.uid)
+      setCompetitionName(selectedCompetition.name)
+    }
+  }, [user, competitions, competitionId])
+
+  if (dataLoading) return <Loading />
 
   return (
     <>
@@ -36,12 +83,18 @@ export default function Competitions() {
               name='competition'
               id='competition'
               className='px-3 py-2 border-2 rounded-md bg-background border-primary'
-              value={selectedCompetition}
-              onChange={(e) => setSelectedCompetition(e.target.value)}
+              value={competitionId}
+              onChange={(e) =>
+                router.push(`/competitions?competition=${e.target.value}`)
+              }
             >
               <option value='none'>Select Competition</option>
-              <option value='id123'>Poopions League</option>
-              <option value='id456'>New competition</option>
+              {competitions &&
+                competitions.map((competition) => (
+                  <option key={competition.id} value={competition.id}>
+                    {competition.name}
+                  </option>
+                ))}
             </select>
           </div>
 
@@ -53,72 +106,95 @@ export default function Competitions() {
           </Link>
         </section>
 
-        <section id='leaderboard2'>
-          <div className='flex flex-row items-center justify-start gap-3'>
-            {isEditingCompetitionName ? (
-              <div className='flex flex-col items-center justify-start gap-2'>
-                <input
-                  type='text'
-                  value={competitionName}
-                  onChange={(e) => setCompetitionName(e.target.value)}
-                  name='competition-name'
-                  className='text-2xl border-b-2 bg-background border-primary'
-                />
-                <div className='flex flex-row items-center justify-between w-full gap-2'>
-                  <button className='flex-1 py-1 rounded-md bg-primary text-foreground'>
-                    Save
-                  </button>
-                  <button
-                    className='flex-1 py-1 rounded-md text-primary bg-foreground'
-                    onClick={() => setIsEditingCompetitionName(false)}
-                  >
-                    Cancel
-                  </button>
+        {error && <h1 className='text-lg'>{error}</h1>}
+
+        {competitionId !== 'none' ? (
+          <section id='competition'>
+            <div className='flex flex-row items-center justify-start gap-3'>
+              {isEditingCompetitionName ? (
+                <div className='flex flex-col items-center justify-start gap-2'>
+                  <input
+                    type='text'
+                    value={competitionName}
+                    onChange={(e) => setCompetitionName(e.target.value)}
+                    name='competition-name'
+                    className='text-2xl border-b-2 bg-background border-primary'
+                  />
+                  <div className='flex flex-row items-center justify-between w-full gap-2'>
+                    <button className='flex-1 py-1 rounded-md bg-primary text-foreground'>
+                      Save
+                    </button>
+                    <button
+                      className='flex-1 py-1 rounded-md text-primary bg-foreground'
+                      onClick={() => setIsEditingCompetitionName(false)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
                 </div>
-              </div>
-            ) : (
-              <>
-                <h2 className='text-2xl'>Poopions League</h2>
-                <button onClick={() => setIsEditingCompetitionName(true)}>
-                  <Pencil className='w-4 h-4' />
-                </button>
-              </>
-            )}
-          </div>
-          <div className='overflow-x-auto'>
-            <table className='w-full border-collapse rounded-lg shadow-md'>
-              <thead>
-                <tr className='text-lg text-foreground'>
-                  <th className='w-1/6 p-3 font-semibold text-left'>Rank</th>
-                  <th className='w-3/6 p-3 font-semibold text-left'>Name</th>
-                  <th className='w-2/6 p-3 font-semibold text-right'>Score</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr className='text-2xl border-b border-b-orange-200 border-opacity-20'>
-                  <td className='p-3 text-left'>1</td>
-                  <td className='p-3 text-lef'>John Doe</td>
-                  <td className='p-3 text-right'>42</td>
-                </tr>
-                <tr className='text-xl border-b border-b-orange-200 border-opacity-20'>
-                  <td className='p-3 text-left'>2</td>
-                  <td className='p-3 text-lef'>Jane Smith</td>
-                  <td className='p-3 text-right'>38</td>
-                </tr>
-                <tr className='text-lg border-b border-b-orange-200 border-opacity-20'>
-                  <td className='p-3 text-left'>3</td>
-                  <td className='p-3 text-lef'>Bob Johnson</td>
-                  <td className='p-3 text-right'>35</td>
-                </tr>
-                <tr className='text-base'>
-                  <td className='p-3 text-left'>3</td>
-                  <td className='p-3 text-lef'>Bob Johnson</td>
-                  <td className='p-3 text-right'>35</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </section>
+              ) : (
+                <>
+                  <h2 className='text-2xl'>{competitionName}</h2>
+                  {canEditCompetition && (
+                    <button onClick={() => setIsEditingCompetitionName(true)}>
+                      <Pencil className='w-4 h-4' />
+                    </button>
+                  )}
+                </>
+              )}
+            </div>
+            <div className='overflow-x-auto'>
+              <table className='w-full border-collapse rounded-lg shadow-md'>
+                <thead>
+                  <tr className='text-lg text-foreground'>
+                    <th className='w-1/6 p-3 font-semibold text-left'>Rank</th>
+                    <th className='w-3/6 p-3 font-semibold text-left'>Name</th>
+                    <th className='w-2/6 p-3 font-semibold text-right'>
+                      Score
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {selectedCompetition &&
+                  selectedCompetition.members.length > 0 ? (
+                    selectedCompetition.members
+                      .sort((a, b) => (b.score || 0) - (a.score || 0))
+                      .map((member, idx) => (
+                        <tr
+                          key={idx}
+                          className={`${
+                            idx === 0
+                              ? 'text-2xl'
+                              : idx === 1
+                              ? 'text-xl'
+                              : idx === 2
+                              ? 'text-lg'
+                              : 'text-base'
+                          } border-b border-b-orange-200 border-opacity-20`}
+                        >
+                          <td className='p-3 text-left'>{idx + 1}</td>
+                          <td className='p-3 text-left'>
+                            {member.fullname || 'Unknown User'}
+                          </td>
+                          <td className='p-3 text-right'>
+                            {member.score || '0'}
+                          </td>
+                        </tr>
+                      ))
+                  ) : (
+                    <tr>
+                      <td colSpan={3} className='p-3 text-center'>
+                        No members in this competition
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </section>
+        ) : (
+          <p>No competition selected or available</p>
+        )}
       </main>
     </>
   )
