92caa9a730bc2b757be1a0e5e46115379ddd9c6e
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2025-03-30 19:14:09 +0100

    Added dynamic competitions information to the dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,6 +7,8 @@ import { useAuthRedirect } from '@/app/hooks/useAuthRedirect'
 import Loading from '@/app/components/Loading'
 import { getDashboardData } from '@/app/dashboard/actions'
 import { getUserData } from '@/app/profile/actions'
+import { getCompetitions } from '@/app/competitions/actions'
+import { Competition } from '@/app/lib/types'
 
 export default function Dashboard() {
   const { loading: authLoading, user } = useAuthState()
@@ -23,6 +25,10 @@ export default function Dashboard() {
   const [dailyRecord, setDailyRecord] = useState<number>(0)
   const [dailyRecordDate, setDailyRecordDate] = useState<string>('')
   const [highestStreakDate, setHighestStreakDate] = useState<string>('')
+
+  const [competitions, setCompetitions] = useState<Competition[] | null>(null)
+  const [selectedCompetition, setSelectedCompetition] =
+    useState<Competition | null>(null)
 
   useEffect(() => {
     if (!user) return
@@ -67,9 +73,39 @@ export default function Dashboard() {
       }
     }
 
+    const fetchCompetitionData = async () => {
+      setDataLoading(true)
+
+      try {
+        const resCompetitions = await getCompetitions(user.uid)
+        const { success, data } = resCompetitions
+
+        if (!success || !data) throw new Error('Competition not found.')
+
+        setCompetitions(data)
+        setDataLoading(false)
+      } catch (error) {
+        console.error(error)
+        setError('Error fetching competition info')
+      }
+    }
+
     fetchDashboardData()
     fetchUserData()
+    fetchCompetitionData()
   }, [user])
+
+  const handleCompetitionChange = (competitionId: string) => {
+    if (!competitions) return
+
+    const comp = competitions.find(
+      (competition) => competition.id === competitionId
+    )
+
+    if (!comp) return
+
+    setSelectedCompetition(comp)
+  }
 
   if (dataLoading) return <Loading />
 
@@ -120,32 +156,91 @@ export default function Dashboard() {
         </section>
 
         <section id='leaderboard' className='mt-8'>
-          <h2 className='text-lg font-bold mb-1'>Leaderboard</h2>
-          <ul>
-            <li className='flex flex-row items-center justify-between text-2xl border-b-2 border-b-primary p-2'>
-              <div className='flex flex-row items-center gap-4'>
-                <span>1</span> André Costa
-              </div>
-              <span>82</span>
-            </li>
-            <li className='flex flex-row items-center justify-between text-xl border-b-2 border-b-primary p-2'>
-              <div className='flex flex-row items-center gap-4'>
-                <span>2</span> Paulo Oliveira
-              </div>
-              <span>80</span>
-            </li>
-            <li className='flex flex-row items-center justify-between text-lg p-2'>
-              <div className='flex flex-row items-center gap-4'>
-                <span>3</span> André Silva
-              </div>
-              <span>71</span>
-            </li>
-            <li className='flex flex-row items-center justify-between text-lg p-2'>
-              <Link href='/competitions?competition=id123'>See more...</Link>
-            </li>
-          </ul>
+          <div className='flex flex-col w-full sm:w-96 mb-2'>
+            <label htmlFor='competition' className='text-lg font-bold mb-1'>
+              Competition
+            </label>
+            <select
+              name='competition'
+              id='competition'
+              className='px-3 py-2 border-2 rounded-md bg-background border-primary'
+              value={selectedCompetition ? selectedCompetition.id : 'none'}
+              onChange={(e) => handleCompetitionChange(e.target.value)}
+            >
+              <option value='none'>Select Competition</option>
+              {competitions &&
+                competitions.map((competition) => (
+                  <option key={competition.id} value={competition.id}>
+                    {competition.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+          {selectedCompetition && (
+            <ul>
+              {selectedCompetition.members.length > 0 ? (
+                <>
+                  {selectedCompetition.members
+                    .sort((a, b) => (b.score || 0) - (a.score || 0))
+                    .slice(0, 3)
+                    .map((member, idx) => (
+                      <li
+                        key={idx}
+                        className={`flex flex-row items-center justify-between border-b border-b-orange-200 border-opacity-20 p-2 ${
+                          idx === 0
+                            ? 'text-2xl'
+                            : idx === 1
+                            ? 'text-xl'
+                            : 'text-lg'
+                        }`}
+                      >
+                        <div className='flex flex-row items-center gap-4'>
+                          <span>{idx + 1}</span>
+                          {member.fullname || 'Unknown User'}
+                        </div>
+                        <span>{member.score || 0}</span>
+                      </li>
+                    ))}
+                  <li className='flex flex-row items-center justify-between text-lg p-2'>
+                    <Link
+                      href={`/competitions?competition=${selectedCompetition.id}`}
+                    >
+                      See more...
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <li>No members in this competition</li>
+              )}
+            </ul>
+          )}
         </section>
       </main>
     </>
   )
 }
+// ;<>
+//   <li className='flex flex-row items-center justify-between text-2xl border-b-2 border-b-primary p-2'>
+//     <div className='flex flex-row items-center gap-4'>
+//       <span>1</span> André Costa
+//     </div>
+//     <span>82</span>
+//   </li>
+//   <li className='flex flex-row items-center justify-between text-xl border-b-2 border-b-primary p-2'>
+//     <div className='flex flex-row items-center gap-4'>
+//       <span>2</span> Paulo Oliveira
+//     </div>
+//     <span>80</span>
+//   </li>
+//   <li className='flex flex-row items-center justify-between text-lg p-2'>
+//     <div className='flex flex-row items-center gap-4'>
+//       <span>3</span> André Silva
+//     </div>
+//     <span>71</span>
+//   </li>
+//   <li className='flex flex-row items-center justify-between text-lg p-2'>
+//     <Link href={`/competitions?competition=${selectedCompetition.id}`}>
+//       See more...
+//     </Link>
+//   </li>
+// </>
